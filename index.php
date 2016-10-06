10913b394329a608b0a220067d5b3cd0a6a257b2
Jpilosel <formation@pilosel.fr>
2016-10-07 00:08:06 +0200

    fond index

--- index.php
+++ index.php
@@ -3,23 +3,14 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <title>Bootstrap 101 Template</title>
+    <title>WeatherMood</title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/weathermood.css" type="text/css" rel="stylesheet">
-    <link href="css/weathermood.css" type="text/css" rel="stylesheetindex">
+    <link rel="stylesheet" type="text/css" href="css/stylesheetindex.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="js/bootstrap.js">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+    <!-- #########################scipt auto Google ###############################-->
     <script src="http://maps.googleapis.com/maps/api/js?sensor=false&amp;libraries=places" type="text/javascript"></script>
     <script type="text/javascript">
 
@@ -42,14 +33,14 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-6">
-        <form action="weathermood.php" method="post">
-            <div>
-                <input id="recherche-ville" type="text" placeholder="Entrez une ville" autocomplete="on" name="ville">
-            </div>
-            <div>
-                <a href="weathermood.php"><imput type="submit" class="btn btn-default" value="validé">Frog</imput></a>
-            </div>
-        </form>
+                    <form action="weathermood.php" method="post">
+                        <div>
+                            <input id="recherche-ville" type="text" placeholder="Entrez une ville" autocomplete="on" name="ville">
+                        </div>
+                        <div>
+                            <a href="weathermood.php"><imput type="submit" class="btn btn-default" value="validé">Frog</imput></a>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
