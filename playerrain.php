8107598caee20a6241fe70eb23fe931241fa5b69
Gy0m <landreaug@gmail.com>
2016-10-06 16:49:46 +0200

    initial commit

---
+++ playerrain.php
@@ -0,0 +1,13 @@
+<html>
+     <head>
+          <meta charset="utf-8">
+          <title>test</title>
+     </head>
+     <body>
+          <div class="container">
+               <div class="col-xs-offset-6 col-xs-3">
+                    <iframe class="player" scrolling="no" frameborder="0" allowTransparency="true" src="http://www.deezer.com/plugins/player?format=classic&autoplay=true&playlist=false&width=700&height=350&color=007FEB&layout=dark&size=medium&type=playlist&id=1891071822&app_id=1" width="700" height="350"></iframe>
+               </div>
+          </div>
+     </body>
+</html>
